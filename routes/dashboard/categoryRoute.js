9c97c162bab41e5659b5bde4c26e5ef61dff0eed
Sanduni Sathsarani <99157473+IT21266546@users.noreply.github.com>
2023-04-24 19:16:38 +0530

    corrections

--- routes/dashboard/categoryRoute.js
+++ routes/dashboard/categoryRoute.js
@@ -2,88 +2,99 @@ const router = require("express").Router();
 const { request } = require("express");
 let categoryModel = require("../../models/dashboard/categoryModel.js");
 
-
-
 //add data to Employee table
 //./Employee/add
 //Post request
 //http://localhost:8050/Employee/add
-router.route("/add").post((req,res)=>{
-    const Cname = req.body.Cname;
-    const CID = req.body.CID;
-    const Price = req.body.Price;
-    const Ctype = req.body.Ctype;
-   
-    const newCategory = new categoryModel({
-        Cname,
-        CID ,
-        Price,
-        Ctype,
-       
-    })
+router.route("/add").post((req, res) => {
+  const Cname = req.body.Cname;
+  const CID = req.body.CID;
+  const Price = req.body.Price;
+  const Ctype = req.body.Ctype;
 
-    newCategory.save().then(()=>{
-        res.json("Category Added")
-    }).catch((err)=>{
-        console.log(err);
+  const newCategory = new categoryModel({
+    Cname,
+    CID,
+    Price,
+    Ctype,
+  });
+
+  newCategory
+    .save()
+    .then(() => {
+      res.json("Category Added");
     })
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 //search Employee
 //http://localhost:8050/Employee/
 //Get Request
-router.route("/").get((req,res)=>{
-    categoryModel.find().then((Category)=>{
-        res.json(Category)
-    }).catch((err)=>{
-        console.log(err)
+router.route("/").get((req, res) => {
+  categoryModel
+    .find()
+    .then((Category) => {
+      res.json(Category);
     })
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 //update
 //http://localhost:8090/Employee/update/:id
 //Put Request
-router.route("/update/:id").put(async (req,res)=>{
-    let userId = req.params.id;
-    const { Cname,CID,Price,Ctype} = req.body;
-    const updateUser = {
-        Cname ,
-        CID,
-        Price,
-        Ctype,
-        
-    }
+router.route("/update/:id").put(async (req, res) => {
+  let userId = req.params.id;
+  const { Cname, CID, Price, Ctype } = req.body;
+  const updateUser = {
+    Cname,
+    CID,
+    Price,
+    Ctype,
+  };
 
-    const update = await categoryModel.findByIdAndUpdate(userId,updateUser).then(()=>{
-        res.status(200).send({status: "Category Updated"})
-    }).catch((err)=>{
-        console.log(err);
-        res.status(500).send({status: "Error with updating data"});
+  const update = await categoryModel
+    .findByIdAndUpdate(userId, updateUser)
+    .then(() => {
+      res.status(200).send({ status: "Category Updated" });
     })
-})
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send({ status: "Error with updating data" });
+    });
+});
 
 //delete Employee
 //http://localhost:8050/Employee/delete/:id
 //Delete Request
-router.route("/delete/:id").delete(async (req, res)=>{
-    let userId = req.params.id;
+router.route("/delete/:id").delete(async (req, res) => {
+  let userId = req.params.id;
 
-    await categoryModel.findByIdAndDelete(userId).then(()=>{
-        res.status(200).send({status: "Category deleted"});
-    }).catch((err)=>{
-        console.log(err);
+  await categoryModel
+    .findByIdAndDelete(userId)
+    .then(() => {
+      res.status(200).send({ status: "Category deleted" });
     })
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 //find one of the Employee
-router.route("/get/:id").get((req,res)=>{
-    let id = req.params.id;
-    categoryModel.find({Cname:id}).then((user)=>{
-        res.json(user)
-    }).catch((err)=>{
-        console.log(err);
+router.route("/get/:id").get((req, res) => {
+  let id = req.params.id;
+  categoryModel
+    .find({ Cname: id })
+    .then((user) => {
+      res.json(user);
     })
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 //Updateone
 // router.route("/updateOne/:id").put(async (req, res) => {
@@ -96,11 +107,9 @@ router.route("/get/:id").get((req,res)=>{
 //         Jobtitle: req.body.Jobtitle || Employee.Jobtitle,
 //         Salary: req.body.Salary || Employee.Salary,
 
-
 //     };
 //     Employee = await EmployeeModel.findByIdAndUpdate(req.params.id, data, { new: true });
 //     res.json(Employee);
 // });
-
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,35 +1,40 @@
-const express = require('express');
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-const getRouter = require('./routes/purchasing/routesGet')
-const postRouter = require('./routes/purchasing/routesPost')
+const getRouter = require("./routes/purchasing/routesGet");
+const postRouter = require("./routes/purchasing/routesPost");
 
 //Dashboard Router {Manager Managemet }
 const DashboardRouter = require("./routes/dashboard/employeeRoute.js");
 
+//dashboard Router {category management}
+const DashboardCatRouter = require("./routes/dashboard/categoryRoute.js");
 
-app.use(bodyParser.json())
-app.use(cors())
-app.use('/purchasingPost', postRouter)
-app.use('/purchasingGet', getRouter)
-app.use("/dashboard",DashboardRouter);
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/purchasingPost", postRouter);
+app.use("/purchasingGet", getRouter);
+app.use("/dashboard", DashboardRouter);
+app.use("/dashboard", DashboardCatRouter);
 
+const PORT = 8080;
+const DB_URL =
+  "mongodb+srv://t5:1234@cluster0.awr06ma.mongodb.net/THRIFT_STORE?retryWrites=true&w=majority";
 
-const PORT = 8080
-const DB_URL = "mongodb+srv://t5:1234@cluster0.awr06ma.mongodb.net/THRIFT_STORE?retryWrites=true&w=majority"
-
-mongoose.connect(DB_URL).then(() => {
-    console.log('Database was connected')
-}).catch((err) => {
-    console.log('Database was not connected, Error orccured')
-    console.log(err)
-})
+mongoose
+  .connect(DB_URL)
+  .then(() => {
+    console.log("Database was connected");
+  })
+  .catch((err) => {
+    console.log("Database was not connected, Error orccured");
+    console.log(err);
+  });
 
 app.listen(PORT, () => {
-    console.log(`Server is Running on ${PORT}`)
-})
-
+  console.log(`Server is Running on ${PORT}`);
+});
